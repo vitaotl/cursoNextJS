b9f300f6caa1d611269c863893943eaba9d538e6
Victor Campos <victor.campos@grancursosonline.com.br>
2022-05-03 14:12:37 -0300

    primeiro commit

---
+++ pages/index.jsx
@@ -0,0 +1,36 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+
+export default HomeScreen
+// import Link from "../src/components/Link"
+// import Footer from "../src/components/patterns/Footer"e
+
+
+
+// function Title({ children, as }) {
+//   const Tag = as
+//   return (
+//     <>
+//       <Tag>
+//         {children}
+//       </Tag>
+//       <style jsx>{`
+//       ${Tag} {
+//         color: red;
+//       }
+//       `}</style>
+//     </>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//         <Title as="h1">Alura Cases - HOME</Title>
+//         <Link href='/faq'>
+//           Ir para o FAQ
+//         </Link>
+//         <Footer />
+//     </div>
+//   )
+// }
